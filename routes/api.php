00e26a0ce50327707173cdd8b0e360b6acc8a2a4
Wylen Joan Lee <57890826+wylenjoan@users.noreply.github.com>
2022-11-04 13:23:16 +0800

    Add new routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\StoryController;
 use App\Http\Controllers\UserController;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,12 +21,27 @@ use Illuminate\Support\Facades\Route;
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/users/username/{user:username}', [UserController::class, 'showByUsername']);
+Route::get('/users/username/{user:username}/stories', [UserController::class, 'showByUsernameWithStories']);
 
+Route::resource('categories', CategoryController::class)->only(['index', 'show']);
+Route::get('/categories/slug/{category:slug}', [CategoryController::class, 'showBySlug']);
+Route::get('/categories/slug/{category:slug}/stories', [CategoryController::class, 'showBySlugWithStories']);
+
+Route::resource('stories', StoryController::class)->only(['index', 'show']);
+Route::get('/stories/slug/{story:slug}', [StoryController::class, 'showBySlug']);
 
 // Private routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::resource('users', UserController::class)->except('post');
-    Route::resource('categories', CategoryController::class);
-    Route::resource('stories', StoryController::class);
+    Route::apiResource('users', UserController::class)->except('post');
+    Route::resource('categories', CategoryController::class)->except(['index', 'show']);
+    Route::resource('stories', StoryController::class)->except(['index', 'show']);
+});
+
+// Fallback route
+Route::fallback(function() {
+    return response()->json([
+        'message' => 'Route not found'
+    ], 404);
 });
